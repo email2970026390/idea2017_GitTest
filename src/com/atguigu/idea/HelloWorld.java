34d7fe8e8516d5c26f523df09ee37ac5986139f4
sunyexin <2970026390@qq.com>
2023-01-06 15:19:34 +0800

    问世程序

---
+++ src/com/atguigu/idea/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.atguigu.idea;
+
+/**
+ * @author sunyexin
+ * @description TODO
+ * @create 2023/1/6 15:17
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld.main");
+    }
+
+}
